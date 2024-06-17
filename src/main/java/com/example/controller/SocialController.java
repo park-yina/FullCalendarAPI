c3c8a3e4c0fd579964a1236ca51036868551ce91
park-yina <kln99988@gmail.com>
2024-06-18 01:07:39 +0900

    Feat:네이버 로그인 get 완성. 정보 받아오기 아직.

--- src/main/java/com/example/controller/SocialController.java
+++ src/main/java/com/example/controller/SocialController.java
@@ -11,7 +11,7 @@ public class SocialController {
 
     @GetMapping("/naver")
     public String naverLogin() {
-
+        //이 부분 url은 변경불가
         return "redirect:/oauth2/authorization/naver";
     }
 }
